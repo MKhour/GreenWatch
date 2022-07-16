aa74ad9b2a1810d0edb14d49cb78411878ec051f
MKhour <madelyn.g.khoury@gmail.com>
2022-07-16 19:13:26 -0400

    Initial Commit

---
+++ GreenWatch/GreenWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenWatchApp.swift
+//  GreenWatch
+//
+//  Created by Madelyn Khoury on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct GreenWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
